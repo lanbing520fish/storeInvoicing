edcff488c69055e79d517ddd99fd6fb5eebf5d98
dingshaohua <345561171@qq.com>
2016-11-11 17:34:48 +0800

    TODO 增加一行

--- view/addPurchase/addPurchase.js
+++ view/addPurchase/addPurchase.js
@@ -1,8 +1,139 @@
 angular
     .module('addPurchaseModule', ['ui.bootstrap'])
-    .controller('conditionQuery', ['$scope', '$timeout', function($scope, $timeout) {
+    .controller('resultCtrl', ['$scope', '$rootScope', '$log', '$timeout', function($scope, $rootScope, $log, $timeout) {
+        // 串号录入
+        $scope.editQueryType = function(index, item) {
+            var obj = _.clone(item);
+            _.set(obj, '$$hashKey', null);
+            $scope.$emit('openSerialNumberModal', index, obj);
+        };
 
+        // 新增一行
+        $scope.addQueryType = function() {
+            $scope.$emit('openEditQueryTypeModal');
+        };
+
+        // 删除
+        $scope.delQueryType = function(index) {
+            _.pullAt(_.get($rootScope, 'modifiedCardPur.itemList'), [index]);
+        };
+
+        // 暂存
+        $scope.saveProcureOrder = function() {
+            httpMethod.saveProcureOrder(_.get($rootScope, 'modifiedCardPur')).then(function(rsp) {
+                $log.log('调用保存采购单接口成功.');
+                if (rsp.success) {
+                    swal({
+                        title: '操作成功!',
+                        text: '保存采购单成功！',
+                        type: 'success'
+                    }, function() {
+                        $timeout(function() {
+                            parent.angular.element(parent.$('#tabs')).scope().removeTab();
+                        });
+                    });
+                } else {
+                    swal("OMG", "保存采购单失败!", "error");
+                }
+            })
+        }
+
+        // 提交
+        $scope.submitProcureOrder = function() {
+            httpMethod.submitProcureOrder(_.get($rootScope, 'modifiedCardPur')).then(function(rsp) {
+                $log.log('调用提交采购单接口成功.');
+                if (rsp.success) {
+                    swal({
+                        title: '操作成功!',
+                        text: '提交采购单成功！',
+                        type: 'success'
+                    }, function() {
+                        $timeout(function() {
+                            parent.angular.element(parent.$('#tabs')).scope().removeTab();
+                        });
+                    });
+                } else {
+                    swal("OMG", "提交采购单失败!", "error");
+                }
+            })
+        }
     }])
+    // 弹出框
+    .controller('addPurchaseModalCtrl', function($scope, $rootScope, $uibModal) {
+        var $ctrl = this,
+            modalInstance;
+        $ctrl.animationsEnabled = true;
+
+        // 新增一行
+        $scope.$on('openEditQueryTypeModal', function(d, data) {
+            $rootScope.queryTypeResultList = [];
+            $ctrl.open(data);
+        });
+
+        // 串号录入
+        $scope.$on('openSerialNumberModal', function(d, index, item) {
+            $ctrl.openSerialNumberModal(index, item);
+        });
+
+        $ctrl.open = function(data) {
+            modalInstance = $uibModal.open({
+                animation: $ctrl.animationsEnabled,
+                ariaLabelledBy: 'new-line-title',
+                ariaDescribedBy: 'new-line-body',
+                templateUrl: 'addNewLine.html',
+                controller: 'addNewLineCtrl',
+                controllerAs: '$ctrl',
+                size: 'lg',
+                resolve: {
+                    items: function() {
+                        return data;
+                    }
+                }
+            });
+        };
+
+        $ctrl.openSerialNumberModal = function(data) {
+            modalInstance = $uibModal.open({
+                animation: $ctrl.animationsEnabled,
+                ariaLabelledBy: 'serial-number-title',
+                ariaDescribedBy: 'serial-number-body',
+                templateUrl: 'serialNumberModal.html',
+                controller: 'serialNumberModalCtrl',
+                controllerAs: '$ctrl',
+                size: 'lg',
+                resolve: {
+                    items: function() {
+                        return data;
+                    }
+                }
+            });
+        };
+
+        $ctrl.toggleAnimation = function() {
+            $ctrl.animationsEnabled = !$ctrl.animationsEnabled;
+        };
+    })
+    .controller('addNewLineCtrl', function($uibModalInstance, $scope, items) {
+        var $ctrl = this;
+        $ctrl.ok = function() {
+            $scope.$broadcast('submitPowerListModal');
+            $uibModalInstance.close();
+        };
+        $ctrl.cancel = function() {
+            $uibModalInstance.dismiss('cancel');
+        };
+    })
+    .controller('serialNumberModalCtrl', function($uibModalInstance, $scope, items) {
+        var $ctrl = this;
+
+        $ctrl.ok = function() {
+            $scope.$broadcast('submitCardRange');
+            $uibModalInstance.close();
+        };
+        $ctrl.cancel = function() {
+            $uibModalInstance.dismiss('cancel');
+        };
+    })
     // 分页控制器
     .controller('paginationCtrl', ['$scope', '$rootScope', '$log', function($scope, $rootScope, $log) {
         $scope.$on('pageChange', function() {
