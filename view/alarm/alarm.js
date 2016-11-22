6ba4033e5c21c187730340b48512c897dc2400fe
mac <mac@MacBook-Pro.local>
2016-11-22 17:33:40 +0800

    no message

--- view/alarm/alarm.js
+++ view/alarm/alarm.js
@@ -1,43 +1,6 @@
 angular
     .module('alarmModule', ['ui.bootstrap'])
     .controller('conditionQuery', ['$scope', '$timeout', function($scope, $timeout) {
-        $scope.conditionQueryForm = {
-            createStartDt: '', //制单日期开始
-            createEndDt: '', //制单日期结束
-        };
-
-        // 时间控件
-        $scope.startDateOptions = {
-            formatYear: 'yy',
-            maxDate: $scope.conditionQueryForm.createEndDt,
-            startingDay: 1
-        };
-        $scope.endDateOptions = {
-            formatYear: 'yy',
-            minDate: $scope.conditionQueryForm.createStartDt,
-            // maxDate: new Date(),
-            startingDay: 1
-        };
-
-        $scope.$watch('conditionQueryForm.createStartDt', function(newValue) {
-            $scope.endDateOptions.minDate = newValue;
-        });
-        $scope.$watch('conditionQueryForm.createEndDt', function(newValue) {
-            $scope.startDateOptions.maxDate = newValue;
-        });
-
-        $scope.startOpen = function() {
-            $timeout(function() {
-                $scope.startPopupOpened = true;
-            });
-        };
-        $scope.endOpen = function() {
-            $timeout(function() {
-                $scope.endPopupOpened = true;
-            });
-        };
-        $scope.startPopupOpened = false;
-        $scope.endPopupOpened = false;
     }])
     // 分页控制器
     .controller('paginationCtrl', ['$scope', '$rootScope', '$log', function($scope, $rootScope, $log) {
